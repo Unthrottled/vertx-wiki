c45099be85a1942dcf1c57fe85b49b12c19c1d03
cyclic-reference <alex@cyclicreference.space>
2017-08-27 12:23:01 -0500

    So this is a thing

--- src/main/java/io/acari/starter/Steve.java
+++ src/main/java/io/acari/starter/Steve.java
@@ -1,14 +1,16 @@
 package io.acari.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 
 public class Steve extends AbstractVerticle {
 
   @Override
-  public void start() {
+  public void start(Future<Void> startupGuy) {
     vertx.createHttpServer()
-      .requestHandler(req -> req.response().end("Hello Vert.x!"))
+      .requestHandler(req -> req.response().end("Hello Vert.x!\n"))
       .listen(6666);
+    startupGuy.complete();
   }
 
 }
