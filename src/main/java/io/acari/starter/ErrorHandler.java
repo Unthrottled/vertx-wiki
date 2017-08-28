f8f47dd2cef925987740310329fd6cb2c0312b36
cyclic-reference <alex@cyclicreference.space>
2017-08-28 15:07:17 -0500

    Re-Fracture

--- src/main/java/io/acari/starter/ErrorHandler.java
+++ src/main/java/io/acari/starter/ErrorHandler.java
@@ -1,56 +1,35 @@
 package io.acari.starter;
 
-import com.google.common.net.MediaType;
 import com.google.inject.Inject;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.templ.TemplateEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class ErrorHandler implements Handler<RoutingContext> {
   private static final Logger LOGGER = LoggerFactory.getLogger(ErrorHandler.class);
-  private final TemplateEngine templateEngine;
+  private final TemplateRenderer templateRenderer;
 
   @Inject
-  public ErrorHandler(TemplateEngine templateEngine) {
-    this.templateEngine = templateEngine;
+  public ErrorHandler(TemplateRenderer templateRenderer) {
+    this.templateRenderer = templateRenderer;
   }
 
 
   @Override
   public void handle(RoutingContext routingContext) {
-    brokenTemplate(routingContext, a -> {
-      if (a.succeeded()) {
-        routingContext.response().putHeader("Content-Type", MediaType.XHTML_UTF_8.type());
-        routingContext.response().end(a.result());
-      } else {
-        broken(routingContext, a);
-      }
-    });
+    brokenTemplate(routingContext);
   }
 
-  public void renderTemplate(RoutingContext routingContext, Handler<AsyncResult<Buffer>> asyncResultHandler, String templateFileName) {
-    templateEngine.render(routingContext, "templates", templateFileName, asyncResultHandler);
-  }
-
-  public <T> void broken(RoutingContext routingContext, AsyncResult<T> asyncResult) {
+  public <T> void brokenTemplate(RoutingContext routingContext, AsyncResult<T> asyncResult) {
     LOGGER.warn("Things Borked ->", asyncResult.cause());
-    routingContext.response()
-      .setStatusCode(500)
-      .end("¯\\_(ツ)_/¯\n");
+    brokenTemplate(routingContext);
   }
 
-  public <T> void brokenTemplate(RoutingContext routingContext, AsyncResult<T> asyncResult, Handler<AsyncResult<Buffer>> asyncResultHandler) {
-    LOGGER.warn("Things Borked ->", asyncResult.cause());
-    brokenTemplate(routingContext, asyncResultHandler);
-  }
-
-  public <T> void brokenTemplate(RoutingContext routingContext, Handler<AsyncResult<Buffer>> asyncResultHandler) {
+  public <T> void brokenTemplate(RoutingContext routingContext) {
     routingContext.response().setStatusCode(500);
-    renderTemplate(routingContext, asyncResultHandler, "/error.ftl");
+    templateRenderer.render(routingContext, "/error.ftl");
   }
 }
--- src/main/java/io/acari/starter/IndexHandler.java
+++ src/main/java/io/acari/starter/IndexHandler.java
@@ -1,14 +1,9 @@
 package io.acari.starter;
 
-import com.google.common.net.MediaType;
 import com.google.inject.Inject;
-import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.templ.FreeMarkerTemplateEngine;
-import io.vertx.ext.web.templ.TemplateEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,25 +14,17 @@ public class IndexHandler implements Handler<RoutingContext> {
   private static final Logger LOGGER = LoggerFactory.getLogger(IndexHandler.class);
 
   private final Database database;
-  private final TemplateEngine templateEngine;
+  private final TemplateRenderer templateRenderer;
   private final ErrorHandler errorHandler;
 
   @Inject
-  public IndexHandler(Database database, TemplateEngine templateEngine, ErrorHandler errorHandler) {
+  public IndexHandler(Database database, TemplateRenderer templateRenderer, ErrorHandler errorHandler) {
     this.database = database;
-    this.templateEngine = templateEngine;
+    this.templateRenderer = templateRenderer;
     this.errorHandler = errorHandler;
   }
 
   public void handle(RoutingContext routingContext) {
-    final Handler<AsyncResult<Buffer>> templateResultHandler = bufferAsyncResult -> {
-      if (bufferAsyncResult.succeeded()) {
-        routingContext.response().putHeader("Content-Type", MediaType.HTML_UTF_8.type());
-        routingContext.response().end(bufferAsyncResult.result());
-      } else {
-        errorHandler.broken(routingContext, bufferAsyncResult);
-      }
-    };
 
     database.executeQuery(ar -> {
       if (ar.succeeded()) {
@@ -55,13 +42,13 @@ public class IndexHandler implements Handler<RoutingContext> {
             routingContext.put("title", "Wiki Home");
             routingContext.put("pages", pages);
             String templateFileName = "/index.ftl";
-            errorHandler.renderTemplate(routingContext, templateResultHandler, templateFileName);
+            templateRenderer.render(routingContext, templateFileName);
           } else {
-            errorHandler.brokenTemplate(routingContext, resultSetAsyncResult, templateResultHandler);
+            errorHandler.handle(routingContext);
           }
         });
       } else {
-        errorHandler.brokenTemplate(routingContext, ar, templateResultHandler);
+        errorHandler.handle(routingContext);
       }
     });
   }
