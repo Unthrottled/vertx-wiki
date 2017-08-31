69d5f79b0f5768c874daf3d8e9efe3e466193953
cyclic-reference <alex@cyclicreference.space>
2017-08-30 19:17:31 -0500

    Deletion Handler

---
+++ src/main/java/io/acari/starter/DeletionHandler.java
@@ -0,0 +1,11 @@
+package io.acari.starter;
+
+import io.vertx.core.Handler;
+import io.vertx.rxjava.ext.web.RoutingContext;
+
+public class DeletionHandler implements Handler<RoutingContext> {
+  @Override
+  public void handle(RoutingContext routingContext) {
+
+  }
+}
