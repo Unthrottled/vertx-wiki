de1d67e8e7cf007e76e9a353f03bc6681eb4f2b4
cyclic-reference <alex@cyclicreference.space>
2017-08-30 19:19:26 -0500

    Some Dependencies

--- src/main/java/io/acari/starter/DeletionHandler.java
+++ src/main/java/io/acari/starter/DeletionHandler.java
@@ -1,11 +1,26 @@
 package io.acari.starter;
 
+import com.google.inject.Inject;
 import io.vertx.core.Handler;
 import io.vertx.rxjava.ext.web.RoutingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DeletionHandler implements Handler<RoutingContext> {
+  private static final Logger LOGGER = LoggerFactory.getLogger(DeletionHandler.class);
+
+  private final Database database;
+  private final ErrorHandler errorHandler;
+
+  @Inject
+  public DeletionHandler(Database database, ErrorHandler errorHandler) {
+    this.database = database;
+    this.errorHandler = errorHandler;
+  }
+
   @Override
   public void handle(RoutingContext routingContext) {
+
 
   }
 }
