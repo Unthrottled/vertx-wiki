50580b83049e90b91839e9787265b73088d490bf
cyclic-reference <alex@cyclicreference.space>
2017-08-31 19:17:02 -0500

    Manuel Deployment of Verticals.

--- src/main/java/io/acari/VertxLauncher.java
+++ src/main/java/io/acari/VertxLauncher.java
@@ -2,15 +2,17 @@ package io.acari;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import io.acari.util.inject.VerticalOrchestrator;
+import io.acari.core.DatabaseVerticle;
+import io.acari.core.HttpVerticle;
 import io.acari.util.inject.VertxModule;
-import io.vertx.core.Future;
+import io.vertx.core.Vertx;
 
 public class VertxLauncher {
 
-  public static void main(String... args){
+  public static void main(String... args) {
     Injector injector = Guice.createInjector(new VertxModule());
-    VerticalOrchestrator verticalOrchestrator = injector.getInstance(VerticalOrchestrator.class);
-    verticalOrchestrator.start(Future.future());
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(injector.getInstance(DatabaseVerticle.class));
+    vertx.deployVerticle(injector.getInstance(HttpVerticle.class));
   }
 }
