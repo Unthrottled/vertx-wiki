afc5a510947745f0397da2ec0cefe634a18f22b4
cyclic-reference <alex@cyclicreference.space>
2017-08-31 19:26:06 -0500

    Http Server Configz.

--- src/main/java/io/acari/core/HttpVerticle.java
+++ src/main/java/io/acari/core/HttpVerticle.java
@@ -11,12 +11,16 @@ import org.slf4j.LoggerFactory;
 
 public class HttpVerticle extends AbstractVerticle {
   private static final Logger LOGGER = LoggerFactory.getLogger(HttpVerticle.class);
+  private static final String CONFIG_HTTP_SERVER_PORT = "http.server.port";
+  private static final String CONFIG_WIKIDB_QUEUE = "wikidb.queue";
+  private static final int CONFIG_HTTP_SERVER_PORT_NUMBER = 8989;
   private final IndexHandler indexHandler;
   private final ErrorHandler errorHandler;
   private final PageHandler pageHandler;
   private final CreationHandler creationHandler;
   private final SaveHandler saveHandler;
   private final DeletionHandler deletionHandler;
+  private String wikiDbQueue = CONFIG_WIKIDB_QUEUE;
 
   @Inject
   public HttpVerticle(IndexHandler indexHandler,
@@ -36,6 +40,7 @@ public class HttpVerticle extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> future) {
+    wikiDbQueue = config().getString(CONFIG_WIKIDB_QUEUE, CONFIG_WIKIDB_QUEUE);
     Router router = Router.router(vertx);
     router.get("/").handler(indexHandler);
     router.get("/error").handler(errorHandler);
@@ -45,9 +50,10 @@ public class HttpVerticle extends AbstractVerticle {
     router.post("/create").handler(creationHandler);
     router.post("/delete").handler(deletionHandler);
 
+    int portNumber = config().getInteger(CONFIG_HTTP_SERVER_PORT, CONFIG_HTTP_SERVER_PORT_NUMBER);
     vertx.createHttpServer()
       .requestHandler(router::accept)
-      .listen(8989, httpServerAsyncResult -> {
+      .listen(portNumber, httpServerAsyncResult -> {
         io.vertx.core.http.HttpServer result = httpServerAsyncResult.result();
         if (httpServerAsyncResult.succeeded()) {
           LOGGER.info("Server listening on port " + result.actualPort());
