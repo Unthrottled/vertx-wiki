f187261dd681ac1b27dcde844829b0e1f33a025f
cyclic-reference <alex@cyclicreference.space>
2017-09-06 18:56:22 -0500

    DB Service Interfacz

--- src/main/java/io/acari/handler/data/WikiDatabaseService.java
+++ src/main/java/io/acari/handler/data/WikiDatabaseService.java
@@ -4,13 +4,23 @@ import io.vertx.codegen.annotations.Fluent;
 import io.vertx.codegen.annotations.ProxyGen;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
-import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 
 @ProxyGen
 public interface WikiDatabaseService {
 
   @Fluent
-  WikiDatabaseService fetchAllPages(Handler<AsyncResult<JsonArray>> resultHandler);
+  WikiDatabaseService fetchAllPages(Handler<AsyncResult<JsonObject>> resultHandler);
 
+  @Fluent
+  WikiDatabaseService fetchPage(Handler<AsyncResult<JsonObject>> resultHandler);
 
+  @Fluent
+  WikiDatabaseService createPage(Handler<AsyncResult<JsonObject>> resultHandler);
+
+  @Fluent
+  WikiDatabaseService savePage(Handler<AsyncResult<JsonObject>> resultHandler);
+
+  @Fluent
+  WikiDatabaseService deletePage(Handler<AsyncResult<JsonObject>> resultHandler);
 }
