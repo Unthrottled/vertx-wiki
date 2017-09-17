3ddf3fb7c22b27b7e2c310e254c38eb495313a15
cyclic-reference <alex@cyclicreference.space>
2017-09-16 20:44:38 -0500

    TS compiles

--- angular4/app/auth/auth.service.js
+++ angular4/app/auth/auth.service.js
@@ -5,21 +5,36 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-var Observable_1 = require("rxjs/Observable");
-require("rxjs/add/observable/of");
-require("rxjs/add/operator/do");
-require("rxjs/add/operator/delay");
+var http_1 = require("@angular/http");
+require("rxjs/add/operator/map");
+require("rxjs/add/operator/catch");
+var host_service_1 = require("../session/host.service");
+var UserPrincipal_model_1 = require("./UserPrincipal.model");
+var ReplaySubject_1 = require("rxjs/ReplaySubject");
 var AuthService = (function () {
-    function AuthService() {
+    function AuthService(http, hostService) {
+        this.http = http;
+        this.hostService = hostService;
         this.isLoggedIn = false;
+        this.currentPrincipal = new ReplaySubject_1.ReplaySubject(1);
     }
     AuthService.prototype.login = function (user) {
         var _this = this;
-        return Observable_1.Observable.create(true)
-            .delay(1000)
-            .do(function (val) { return _this.isLoggedIn = val; });
+        return this.http.post(this.hostService.fetchUrl() + '/api/token', user)
+            .map(function (response) {
+            return response && response.json ?
+                response.json() : '';
+        })
+            .map(function (json) { return new UserPrincipal_model_1.UserPrincipal(json); })
+            .flatMap(function (prince) {
+            _this.currentPrincipal.next(prince);
+            return _this.currentPrincipal;
+        });
     };
     AuthService.prototype.logout = function () {
         this.isLoggedIn = false;
@@ -27,7 +42,8 @@ var AuthService = (function () {
     return AuthService;
 }());
 AuthService = __decorate([
-    core_1.Injectable()
+    core_1.Injectable(),
+    __metadata("design:paramtypes", [http_1.Http, host_service_1.HostService])
 ], AuthService);
 exports.AuthService = AuthService;
 //# sourceMappingURL=auth.service.js.map
--- angular4/app/auth/auth.service.ts
+++ angular4/app/auth/auth.service.ts
@@ -1,25 +1,41 @@
 import {Injectable} from "@angular/core";
 
 import {Observable} from "rxjs/Observable";
-import "rxjs/add/observable/of";
-import "rxjs/add/operator/do";
-import "rxjs/add/operator/delay";
+import {Http, Response} from "@angular/http";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 import {User} from "./user.model";
+import {HostService} from "../session/host.service";
+import {UserPrincipal} from "./UserPrincipal.model";
+import {ReplaySubject} from "rxjs/ReplaySubject";
 
 @Injectable()
 export class AuthService {
   isLoggedIn = false;
+  private currentPrincipal = new ReplaySubject<UserPrincipal>(1);
 
   // store the URL so we can redirect after logging in
   redirectUrl: string;
 
-  login(user: User): Observable<boolean> {
-    return Observable.create(true)
-      .delay(1000)
-      .do((val: boolean) => this.isLoggedIn = val);
+  constructor(private http: Http, private hostService: HostService) {
+
+  }
+
+  login(user: User): Observable<UserPrincipal> {
+    return this.http.post(this.hostService.fetchUrl() + '/api/token', user)
+      .map((response: Response) => {
+        return response && response.json ?
+          response.json() : ''
+      })
+      .map(json => new UserPrincipal(json))
+      .flatMap((prince: UserPrincipal) => {
+        this.currentPrincipal.next(prince);
+        return this.currentPrincipal;
+      });
   }
 
   logout(): void {
     this.isLoggedIn = false;
+    this.currentPrincipal = new ReplaySubject<UserPrincipal>();
   }
 }
