3b0a66ea59a03dcbf0253aa3c40941d416cd59b5
cyclic-reference <alex@cyclicreference.space>
2017-09-17 17:31:33 -0500

    page min model

---
+++ angular4/app/pages/Page.min.model.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by alex on 9/17/17.
+ */
+
+export class PageMin {
+  private _id: string;
+  private _name: string;
+
+  constructor(pageMin: any){
+    this._id = pageMin.id;
+    this._name = name;
+  }
+
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+}
