11e1468677003d637ce0780f466627abdb23ac70
cyclic-reference <alex@cyclicreference.space>
2017-09-21 19:06:18 -0500

    deez things

--- angular4/app/util/backend.service.js
+++ angular4/app/util/backend.service.js
@@ -43,6 +43,7 @@ var BackendService = (function () {
             .map(function (response) { return new StatusPayload_model_1.StatusPayload(response.json()); });
     };
     BackendService.prototype.createPage = function (pageName, pageBody) {
+        console.log(pageName);
         return this.httpPost("api/pages", { "name": pageName, "markdown": pageBody })
             .map(function (response) { return new StatusPayload_model_1.StatusPayload(response.json()); });
     };
