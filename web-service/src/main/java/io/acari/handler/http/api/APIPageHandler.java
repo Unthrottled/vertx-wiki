aa2f44bad1d896bfbe393287904efcd40d2b9de2
cyclic-reference <alex@cyclicreference.space>
2017-09-24 12:46:36 -0500

    gotta work out docker kinks

--- web-service/src/main/java/io/acari/handler/http/api/APIPageHandler.java
+++ web-service/src/main/java/io/acari/handler/http/api/APIPageHandler.java
@@ -2,8 +2,6 @@ package io.acari.handler.http.api;
 
 import com.github.rjeschke.txtmark.Processor;
 import com.google.inject.Inject;
-import io.acari.handler.Config;
-import io.acari.util.ChainableOptional;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.Message;
