e3fae2ec1640a1ea6b22610c710ffc4962d576d2
cyclic-reference <alex@cyclicreference.space>
2017-09-26 12:51:08 -0500

    so i needed a js in my popper

--- angular4/src/vendor.ts
+++ angular4/src/vendor.ts
@@ -18,4 +18,4 @@ import 'jquery';
 import 'angular2-ui-switch';
 import 'angular2-markdown';
 import 'angular2-notifications';
-import 'bootstrap'
+import 'bootstrap';
--- angular4/webpack.config.js
+++ angular4/webpack.config.js
@@ -85,6 +85,15 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery',
+          'window.jQuery': 'jquery',
+          Popper: ['popper.js', 'default'],
+          // In case you imported plugins individually, you must also require them here:
+          Util: "exports-loader?Util!bootstrap/js/dist/util",
+          Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown"
+        }),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
