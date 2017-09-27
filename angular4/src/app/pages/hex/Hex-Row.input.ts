9a19594d2d6511cdcb4de083b302f95a4779e19b
cyclic-reference <alex@cyclicreference.space>
2017-09-27 11:41:45 -0500

    Config binding

---
+++ angular4/src/app/pages/hex/Hex-Row.input.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by alex on 9/27/17.
+ */
+
+export interface HexRowInput {
+  even?: boolean;
+  space?: number;
+}
--- angular4/src/app/pages/hex/hex-row.component.ts
+++ angular4/src/app/pages/hex/hex-row.component.ts
@@ -5,23 +5,34 @@ import {Component, Input} from "@angular/core";
 
 import "./hex-row.htm";
 import {PageMin} from "../Page.min.model";
+import {HexRowInput} from "./Hex-Row.input";
 @Component({
   selector: 'hex-row',
   templateUrl: './templates/hex-row.htm'
 })
 export class HexRowComponent {
   private _pages: PageMin[] = [];
+  private _config: HexRowInput;
 
   constructor() {
   }
 
 
-  @Input
+  @Input()
   get pages(): PageMin[] {
     return this._pages;
   }
 
   set pages(value: PageMin[]) {
     this._pages = value;
+  }
+
+  @Input()
+  get config(): HexRowInput {
+    return this._config;
+  }
+
+  set config(value: HexRowInput) {
+    this._config = value;
   }
 }
