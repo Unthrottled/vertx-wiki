8166a4d120568e87751024366822eac74f713635
cyclic-reference <alex@cyclicreference.space>
2017-10-02 12:25:02 -0500

    still does not compile

--- web-service/src/main/java/io/acari/handler/data/SaveHandler.java
+++ web-service/src/main/java/io/acari/handler/data/SaveHandler.java
@@ -2,11 +2,12 @@ package io.acari.handler.data;
 
 import io.acari.core.Queries;
 import io.acari.util.ChainableOptional;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.sql.SQLConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,10 +15,10 @@ import org.slf4j.LoggerFactory;
 public class SaveHandler implements Handler<Message<JsonObject>> {
   private static final Logger LOGGER = LoggerFactory.getLogger(SaveHandler.class);
 
-  private final JDBCClient jdbcClient;
+  private final MongoClient mongoClient;
 
-  public SaveHandler(JDBCClient jdbcClient) {
-    this.jdbcClient = jdbcClient;
+  public SaveHandler(MongoClient mongoClient) {
+    this.mongoClient = mongoClient;
   }
 
   @Override
@@ -25,27 +26,21 @@ public class SaveHandler implements Handler<Message<JsonObject>> {
     JsonObject request = message.body();
     ChainableOptional.ofNullable(request.getString("name"))
       .ifPresent(id -> ChainableOptional.ofNullable(request.getString("content"))
-        .ifPresent(content ->
-          jdbcClient.getConnection(aConn -> {
-            if (aConn.succeeded()) {
-              SQLConnection connection = aConn.result();
-              connection.updateWithParams(
-                Queries.SqlQueries.SAVE_PAGE.getValue(),
-                new JsonArray().add(content).add(id),
-                aRes -> {
-                  if (aRes.succeeded()) {
-                    LOGGER.info(aRes.result().toJson().encode());
-                    message.reply(new JsonObject().put("status", "gewd"));
-                  } else {
-                    message.fail(ErrorCodes.DB_ERROR.ordinal(), aRes.cause().getMessage());
-                  }
-                })
-                .commit(voidAsyncResult -> LOGGER.info("Save page commit!"))
-                .close(v -> connection.close());
-            } else {
-              message.fail(ErrorCodes.DB_ERROR.ordinal(), aConn.cause().getMessage());
-            }
-          }))
+        .ifPresent(content -> {
+          JsonObject query = new JsonObject().put("name", "name");
+          JsonObject update = new JsonObject().put("$set", new JsonObject().put("content", content));
+          mongoClient.updateCollection("pages", query, update, aConn -> {
+            ChainableOptional.of(aConn)
+              .filter(AsyncResult::succeeded)
+              .ifPresent(conRes -> {
+                message.reply(new JsonObject().put("status", "gewd"));
+              })
+              .orElseDo(() -> {
+                LOGGER.warn("Ohh shit", aConn.cause().getMessage());
+                message.fail(ErrorCodes.DB_ERROR.ordinal(), aConn.cause().getMessage());
+              });
+          });
+        })
         .orElseDo(() -> fourHundred(message, "No Title Provided, Bruv."))
       ).orElseDo(() -> fourHundred(message, "No Id Provided, Bruv."));
   }
