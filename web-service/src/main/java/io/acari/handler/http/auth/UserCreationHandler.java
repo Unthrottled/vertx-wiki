440542f393709280d6ff8f8130229b8fa4d179a3
cyclic-reference <alex@cyclicreference.space>
2017-10-02 19:28:44 -0500

    some things to do

--- web-service/src/main/java/io/acari/handler/http/auth/UserCreationHandler.java
+++ web-service/src/main/java/io/acari/handler/http/auth/UserCreationHandler.java
@@ -1,10 +1,14 @@
 package io.acari.handler.http.auth;
 
 import io.vertx.core.Handler;
+import io.vertx.core.json.JsonArray;
 import io.vertx.ext.auth.mongo.MongoAuth;
 import io.vertx.ext.web.RoutingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserCreationHandler implements Handler<RoutingContext> {
   private static final Logger LOGGER = LoggerFactory.getLogger(UserCreationHandler.class);
@@ -17,5 +21,13 @@ public class UserCreationHandler implements Handler<RoutingContext> {
   @Override
   public void handle(RoutingContext routingContext) {
 
+    List<String> roles = new JsonArray().stream().map(s->(String)s).collect(Collectors.toList());
+    List<String> permissions = new JsonArray().stream().map(s->(String)s).collect(Collectors.toList());
+    mongoAuth.insertUser("steve",
+      "passy",
+      roles,
+      permissions, stringAsyncResult -> {
+
+      });
   }
 }
