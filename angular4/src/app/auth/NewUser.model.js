ee78156466202f36b4cc2e316741e3493843aee4
cyclic-reference <alex@cyclicreference.space>
2017-10-03 15:48:27 -0500

    deez

---
+++ angular4/src/app/auth/NewUser.model.js
@@ -0,0 +1,15 @@
+/**
+ * Created by alex on 10/3/17.
+ */
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var NewUser = (function () {
+    function NewUser(login, password, permissions) {
+        this.login = login;
+        this.password = password;
+        this.permissions = permissions;
+    }
+    return NewUser;
+}());
+exports.NewUser = NewUser;
+//# sourceMappingURL=NewUser.model.js.map
