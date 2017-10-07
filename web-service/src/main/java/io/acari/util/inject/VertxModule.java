8371fc83ee85e84b0e1185189456634ffab159b8
cyclic-reference <alex@cyclicreference.space>
2017-10-06 19:30:26 -0500

    Good bye artifacts of server side rendering

--- web-service/src/main/java/io/acari/util/inject/VertxModule.java
+++ web-service/src/main/java/io/acari/util/inject/VertxModule.java
@@ -1,9 +1,7 @@
 package io.acari.util.inject;
 
 import com.google.inject.AbstractModule;
-import io.acari.util.TemplateEngineProvider;
 import io.vertx.core.Vertx;
-import io.vertx.ext.web.templ.TemplateEngine;
 
 /**
  * Forged in the flames of battle by alex.
@@ -12,6 +10,5 @@ public class VertxModule extends AbstractModule {
   protected void configure() {
     //add configuration logic here
     bind(Vertx.class).toProvider(VertxProvider.class);
-    bind(TemplateEngine.class).toProvider(TemplateEngineProvider.class);
   }
 }
