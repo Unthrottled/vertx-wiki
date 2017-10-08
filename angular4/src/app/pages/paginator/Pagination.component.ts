7de5a463bc6959be215d3e05d54b10c4489f33dd
cyclic-reference <alex@cyclicreference.space>
2017-10-08 18:29:45 -0500

    inputs and outputs

--- angular4/src/app/pages/paginator/Pagination.component.ts
+++ angular4/src/app/pages/paginator/Pagination.component.ts
@@ -1,16 +1,50 @@
 /**
  * Created by alex on 9/17/17.
  */
-import {Component} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 import "./paginator.html";
+import {PaginationPage} from "./PaginationPage";
 @Component({
   selector: 'paginator',
   templateUrl: './templates/paginator.html'
 })
 export class PaginatorComponent {
+  @Output()
+  onPageChanged = new EventEmitter<PaginationPage>();
 
-  // @Output
-  // pageChanged =
+  private _currentPage: PaginationPage;
+  private _totalPages: number;
+  private _itemsPerPage: number = 100;
+
+
   constructor() {
+  }
+
+
+  @Input()
+  get currentPage(): PaginationPage {
+    return this._currentPage;
+  }
+
+  set currentPage(value: PaginationPage) {
+    this._currentPage = value;
+  }
+
+  @Input()
+  get totalPages(): number {
+    return this._totalPages;
+  }
+
+  set totalPages(value: number) {
+    this._totalPages = value;
+  }
+
+  @Input()
+  get itemsPerPage(): number {
+    return this._itemsPerPage;
+  }
+
+  set itemsPerPage(value: number) {
+    this._itemsPerPage = value;
   }
 }
---
+++ angular4/src/app/pages/paginator/PaginationPage.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by alex on 10/8/17.
+ */
+
+
+export class PaginationPage {
+
+}
