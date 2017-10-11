67136e7b776b366b70e2ae6d35aeea0adfc1b82d
cyclic-reference <alexsimons9999@gmail.com>
2017-10-11 06:29:52 -0500

    New frontend model

---
+++ angular4/src/app/pages/LastModfied.model.ts
@@ -0,0 +1,20 @@
+
+export class LastModfied {
+  private _userName: string;
+  private _timeStamp: Date;
+
+
+  constructor(response: any) {
+    this._userName = response.userName;
+    this._timeStamp = new Date(response.timeStamp);
+  }
+
+
+  get userName(): string {
+    return this._userName;
+  }
+
+  get timeStamp(): Date {
+    return this._timeStamp;
+  }
+}
