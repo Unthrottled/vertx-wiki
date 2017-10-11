ca136567664ae76edc030f1819509e5671f2d850
cyclic-reference <alex@cyclicreference.space>
2017-10-10 19:06:58 -0500

    this also looks sane

--- web-service/src/main/java/io/acari/handler/data/SaveHandler.java
+++ web-service/src/main/java/io/acari/handler/data/SaveHandler.java
@@ -9,6 +9,8 @@ import io.vertx.ext.mongo.MongoClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Instant;
+
 public class SaveHandler implements Handler<Message<JsonObject>> {
   private static final Logger LOGGER = LoggerFactory.getLogger(SaveHandler.class);
 
@@ -22,22 +24,28 @@ public class SaveHandler implements Handler<Message<JsonObject>> {
   public void handle(Message<JsonObject> message) {
     JsonObject request = message.body();
     ChainableOptional.ofNullable(request.getString("name"))
-      .ifPresent(id -> ChainableOptional.ofNullable(request.getString("content"))
-        .ifPresent(content -> {
-          JsonObject query = new JsonObject().put("name", id);
-          JsonObject update = new JsonObject().put("$set", new JsonObject().put("content", content));
-          mongoClient.updateCollection("pages", query, update, aConn -> {
-            ChainableOptional.of(aConn)
-              .filter(AsyncResult::succeeded)
-              .ifPresent(conRes -> {
-                message.reply(new JsonObject().put("status", "gewd"));
-              })
-              .orElseDo(() -> {
-                LOGGER.warn("Ohh shit", aConn.cause().getMessage());
-                message.fail(ErrorCodes.DB_ERROR.ordinal(), aConn.cause().getMessage());
-              });
-          });
-        })
+      .ifPresent(id -> ChainableOptional.ofNullable(request.getString("username"))
+        .ifPresent(userName -> ChainableOptional.ofNullable(request.getString("content"))
+          .ifPresent(content -> {
+            JsonObject query = new JsonObject().put("name", id);
+            JsonObject update = new JsonObject().put("$set", new JsonObject()
+              .put("content", content)
+              .put("lastModified", new JsonObject()
+                .put("userName", userName)
+                .put("timeStamp", Instant.now().toEpochMilli())));
+            mongoClient.updateCollection("pages", query, update, aConn -> {
+              ChainableOptional.of(aConn)
+                .filter(AsyncResult::succeeded)
+                .ifPresent(conRes -> {
+                  message.reply(new JsonObject().put("status", "gewd"));
+                })
+                .orElseDo(() -> {
+                  LOGGER.warn("Ohh shit", aConn.cause().getMessage());
+                  message.fail(ErrorCodes.DB_ERROR.ordinal(), aConn.cause().getMessage());
+                });
+            });
+          }))
+        .orElseDo(() -> fourHundred(message, "No User name Provided, Bruv."))
         .orElseDo(() -> fourHundred(message, "No Title Provided, Bruv."))
       ).orElseDo(() -> fourHundred(message, "No Id Provided, Bruv."));
   }
