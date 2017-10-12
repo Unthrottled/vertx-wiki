fee0bdd0846219b8ca0c4e91802ae2e27742b556
cyclic-reference <alex@cyclicreference.space>
2017-10-12 15:55:08 -0500

    remove some dead things

--- startVerticle.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gradle clean shadowJar
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8990 -jar web-service/build/libs/web-service-0.0.1-fat.jar run io.acari.starter.VertxLauncher
