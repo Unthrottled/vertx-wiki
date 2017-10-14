8dbc7c05cf120259f639e57fbf910010448f23a9
cyclic-reference <alex@cyclicreference.space>
2017-10-14 16:54:17 -0500

    find one

--- web-service/src/main/java/io/acari/handler/data/UserExistsHandler.java
+++ web-service/src/main/java/io/acari/handler/data/UserExistsHandler.java
@@ -9,33 +9,42 @@ import io.vertx.ext.mongo.MongoClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Objects;
+
 public class UserExistsHandler implements Handler<Message<JsonObject>> {
-  private static final Logger LOGGER = LoggerFactory.getLogger(UserExistsHandler.class);
-  private final MongoClient mongoClient;
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserExistsHandler.class);
+    private final MongoClient mongoClient;
 
-  public UserExistsHandler(MongoClient mongoClient) {
-    this.mongoClient = mongoClient;
-  }
+    public UserExistsHandler(MongoClient mongoClient) {
+        this.mongoClient = mongoClient;
+    }
 
-  @Override
-  public void handle(Message<JsonObject> message) {
-    ChainableOptional.ofNullable(message.body().getString("page"))//it's page cuz I'm a slob
-      .ifPresent(pago -> mongoClient.find("user", new JsonObject()
-        .put("username", pago), asyncResultHandler -> {
-        ChainableOptional.of(asyncResultHandler)
-          .filter(AsyncResult::succeeded)
-          .ifPresent(result -> message.reply(result.result()
-            .stream()
-            .peek(a -> LOGGER.info(result.result().toString()))
-            .findFirst()
-            .map(jsonArray -> new JsonObject()
-              .put("exists", !jsonArray.getString("_id").isEmpty()))
-            .orElse(new JsonObject()
-              .put("exists", false))))
-          .orElseDo(() -> {
-            LOGGER.warn("Ohhhhhh Sheeit", asyncResultHandler.cause().getMessage());
-            message.fail(500, asyncResultHandler.cause().getMessage());
-          });
-      })).orElseDo(() -> message.fail(400, "No Path Provided, bruv."));
-  }
+    @Override
+    public void handle(Message<JsonObject> message) {
+        ChainableOptional.ofNullable(message.body().getString("page"))//it's page cuz I'm a slob
+                .ifPresent(pago -> {
+                    JsonObject username = new JsonObject()
+                            .put("username", pago);
+                    mongoClient.findOne("user", username, username,
+                            asyncResultHandler -> {
+                                ChainableOptional.of(asyncResultHandler)
+                                        .filter(AsyncResult::succeeded)
+                                        .map(AsyncResult::result)
+                                        .ifPresent(result -> ChainableOptional.ofNullable(result)
+                                                .filter(Objects::nonNull)
+                                                .ifPresent(res -> message.reply(getExists(true)))
+                                                .orElseDo(() -> lulNup(message)))
+                                        .orElseDo(() -> lulNup(message));
+                            });
+                }).orElseDo(() -> message.fail(400, "No Path Provided, bruv."));
+    }
+
+    private void lulNup(Message<JsonObject> message) {
+        message.reply(getExists(false));
+    }
+
+    private JsonObject getExists(boolean value) {
+        return new JsonObject()
+                .put("exists", value);
+    }
 }
