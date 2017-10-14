1d7a829045dda2f864a8de13b9b9d4771bc90804
cyclic-reference <alex@cyclicreference.space>
2017-10-14 16:12:25 -0500

    Hey that's a favicon

--- angular4/webpack.config.js
+++ angular4/webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'file-loader?name=assets/[name].[hash].[ext]',
+        loader: 'file-loader?name=[name].[hash].[ext]',
         exclude: [/node_modules/, /build/, /dist/, /gradle/]
       },
       {
