9d72cd5747cdfff32f0c03b0b550e9b995d2deff
cyclic-reference <alex@cyclicreference.space>
2017-10-15 13:34:15 -0500

    need to mod dis

--- web-service/src/main/java/io/acari/handler/data/UserUpdateHandler.java
+++ web-service/src/main/java/io/acari/handler/data/UserUpdateHandler.java
@@ -4,6 +4,7 @@ import io.acari.util.ChainableOptional;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
 import org.slf4j.Logger;
@@ -24,13 +25,13 @@ public class UserUpdateHandler implements Handler<Message<JsonObject>> {
   public void handle(Message<JsonObject> message) {
     JsonObject request = message.body();
     ChainableOptional.ofNullable(request.getString("username"))
-        .ifPresent(username -> ChainableOptional.ofNullable(request.getJsonArray("permissions"))
-            .ifPresent(permissions -> {
+        .ifPresent(username -> ChainableOptional.ofNullable(request.getString("role"))
+            .ifPresent(role -> {
               JsonObject query = new JsonObject().put("username", username);
               JsonObject update = new JsonObject().put("$set", new JsonObject()
-                  .put("permissions", permissions));
+                  .put("roles", new JsonArray().add(role)));
               mongoClient.findOneAndUpdate("user", query, update, getAsyncResultHandler(message));
-            }).orElseDo(() -> fourHundred(message, "No Title Provided, Bruv."))
+            }).orElseDo(() -> fourHundred(message, "No role Provided, Bruv."))
         ).orElseDo(() -> fourHundred(message, "No username Provided, Bruv."));
   }
 
