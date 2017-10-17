c22c70ecb07675691e4e6423731dba5460801b5b
cyclic-reference <alexsimons9999@gmail.com>
2017-10-17 06:53:26 -0500

    some cleanup

--- angular4/src/app/pages/archive/Archives.component.ts
+++ angular4/src/app/pages/archive/Archives.component.ts
@@ -3,44 +3,38 @@
  */
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
-import {PageMin} from "../Page.min.model";
 import "./pages.list.htm";
-import {PagePayload} from "../PagePayload.model";
 import {PageMetaData} from "../metadata.model";
-import {AuthService} from "../../auth/auth.service";
-import {Observable} from "rxjs/Observable";
 import {ArchivePageMin} from "./ArchivePageMin";
+import {ArchivesPayload} from "./ArchivesPayload.model";
+
 @Component({
-  selector: 'pages-list',
-  templateUrl: './templates/pages.list.htm'
+    selector: 'pages-list',
+    templateUrl: './templates/pages.list.htm'
 })
 export class ArchivesComponent implements OnInit {
-  pages: PageMin[] = [];
-  metaData: PageMetaData;
+    pages: ArchivePageMin[] = [];
+    metaData: PageMetaData;
 
-  constructor(private router: ActivatedRoute, private realRouter: Router, private authService: AuthService) {
-  }
+    constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+    }
 
-  ngOnInit(): void {
-    this.router.data.subscribe((data: { pages: PagePayload }) => {
-      this.pages = data.pages.pages;
-      this.metaData = data.pages.metadata;
-    });
-  }
+    ngOnInit(): void {
+        this.activatedRoute.data.subscribe((data: { pages: ArchivesPayload }) => {
+            this.pages = data.pages.pages;
+            this.metaData = data.pages.metadata;
+        });
+    }
 
-  reRoute(pageArchive: ArchivePageMin): void {
-    this.realRouter.navigate(['/archive/' + pageArchive.id]);
-  }
+    reRoute(pageArchive: ArchivePageMin): void {
+        this.router.navigate(['/archive/' + pageArchive.id]);
+    }
 
-  reRouteMain(pageArchive: ArchivePageMin): void {
-    this.realRouter.navigate(['/archive/' + pageArchive.id]);
-  }
+    reRouteMain(pageArchive: ArchivePageMin): void {
+        this.router.navigate(['/archive/' + pageArchive.id]);
+    }
 
-  hasPages(): boolean {
-    return this.pages.length > 0;
-  }
-
-  canCreate(): Observable<boolean> {
-    return this.authService.canCreate();
-  }
+    hasPages(): boolean {
+        return this.pages.length > 0;
+    }
 }
