3bf4f899496784867eaca37a1141ba43e62cfaad
DATBOI <alex@cyclicreference.space>
2018-01-02 11:54:31 -0600

    this looks okay for now

--- angular4/src/app/app.component.js
+++ angular4/src/app/app.component.js
@@ -5,17 +5,31 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 require("./app.component.htm");
+var router_1 = require("@angular/router");
+var ReplaySubject_1 = require("rxjs/ReplaySubject");
 var AppComponent = /** @class */ (function () {
-    function AppComponent() {
+    function AppComponent(router) {
+        var _this = this;
+        this.router = router;
+        this.loadEnd = new ReplaySubject_1.ReplaySubject(1);
+        router.events
+            .filter(function (event) { return event instanceof router_1.NavigationEnd; })
+            .subscribe(function (end) {
+            _this.loadEnd.next(true);
+        });
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'angular-application',
             template: require('./app.component.htm')
-        })
+        }),
+        __metadata("design:paramtypes", [router_1.Router])
     ], AppComponent);
     return AppComponent;
 }());
--- angular4/src/app/app.component.ts
+++ angular4/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import {Component} from "@angular/core";
 import "./app.component.htm";
+import {NavigationEnd, Router} from "@angular/router";
+import {ReplaySubject} from "rxjs/ReplaySubject";
 
 @Component({
     selector: 'angular-application',
     template: require('./app.component.htm')
 })
 export class AppComponent {
+    loadEnd: ReplaySubject<boolean> = new ReplaySubject<boolean>(1);
 
+    constructor(private router: Router){
+        router.events
+            .filter(event => event instanceof NavigationEnd)
+            .subscribe(end=>{
+                this.loadEnd.next(true);
+            })
+    }
 }
